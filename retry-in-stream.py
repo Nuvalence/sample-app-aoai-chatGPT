3ec9699d468c91753d55f5e2c93279121a09e7d6
aantenangeli <aantenageli@nuvalence.io>
2023-08-09 20:08:22 -0500

    Partial commit

--- retry-in-stream.py
+++ retry-in-stream.py
@@ -6,7 +6,7 @@ import random
 import requests
 import time
 import openai
-from flask import Flask, Response, request, jsonify, send_from_directory
+from flask import Flask, Response, request, jsonify, send_from_directory, copy_current_request_context
 from dotenv import load_dotenv
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
@@ -31,7 +31,8 @@ if 'APPLICATIONINSIGHTS_CONNECTION_STRING' in os.environ and 'APPLICATIONINSIGHT
     instrumentation_key = os.getenv('APPLICATIONINSIGHTS_INSTRUMENTATION_KEY')
     logger.addHandler(AzureLogHandler(connection_string=connection_string, instrumentation_key=instrumentation_key))
 else:
-    logger.info("APPLICATIONINSIGHTS_CONNECTION_STRING and _INSTRUMENTATION_KEY defined not defined, AzureLogHandler will not be initialized")
+    logger.info(
+        "APPLICATIONINSIGHTS_CONNECTION_STRING and _INSTRUMENTATION_KEY defined not defined, AzureLogHandler will not be initialized")
 
 
 # -----------------------------------------------------------------------------
@@ -74,11 +75,12 @@ AZURE_OPENAI_TEMPERATURE = os.environ.get("AZURE_OPENAI_TEMPERATURE", 0)
 AZURE_OPENAI_TOP_P = os.environ.get("AZURE_OPENAI_TOP_P", 1.0)
 AZURE_OPENAI_MAX_TOKENS = os.environ.get("AZURE_OPENAI_MAX_TOKENS", 1000)
 AZURE_OPENAI_STOP_SEQUENCE = os.environ.get("AZURE_OPENAI_STOP_SEQUENCE")
-AZURE_OPENAI_SYSTEM_MESSAGE = os.environ.get("AZURE_OPENAI_SYSTEM_MESSAGE", "You are an AI assistant that helps people find information.")
+AZURE_OPENAI_SYSTEM_MESSAGE = os.environ.get("AZURE_OPENAI_SYSTEM_MESSAGE",
+                                             "You are an AI assistant that helps people find information.")
 AZURE_OPENAI_PREVIEW_API_VERSION = os.environ.get("AZURE_OPENAI_PREVIEW_API_VERSION", "2023-06-01-preview")
 AZURE_OPENAI_STREAM = os.environ.get("AZURE_OPENAI_STREAM", "true")
-SHOULD_STREAM = True if AZURE_OPENAI_STREAM.lower() == "true" else False
 
+SHOULD_STREAM = True if AZURE_OPENAI_STREAM.lower() == "true" else False
 
 # -----------------------------------------------------------------------------
 # Load balanced endpoints
@@ -137,11 +139,11 @@ def should_use_data():
     return False
 
 
-def prepare_body_headers_with_data(request, ndx):
-    request_messages = request.json["messages"]
-    openai_key = openai_keys[ndx]
-    openai_resource = openai_resources[ndx]
-    openai_model = openai_models[ndx]
+def prepare_body_headers_with_data(request_messages, ndx):
+    # request_messages = request.json["messages"]
+    open_ai_key = openai_keys[ndx]
+    open_ai_resource = openai_resources[ndx]
+    open_ai_model = openai_models[ndx]
 
     body = {
         "messages": request_messages,
@@ -173,7 +175,7 @@ def prepare_body_headers_with_data(request, ndx):
         ]
     }
 
-    chatgpt_url = f"https://{openai_resource}.openai.azure.com/openai/deployments/{openai_model}"
+    chatgpt_url = f"https://{open_ai_resource}.openai.azure.com/openai/deployments/{open_ai_model}"
     if is_chat_model():
         chatgpt_url += "/chat/completions?api-version=2023-03-15-preview"
     else:
@@ -181,17 +183,16 @@ def prepare_body_headers_with_data(request, ndx):
 
     headers = {
         'Content-Type': 'application/json',
-        'api-key': openai_key,
+        'api-key': open_ai_key,
         'chatgpt_url': chatgpt_url,
-        'chatgpt_key': openai_key,
+        'chatgpt_key': open_ai_key,
         "x-ms-useragent": "GitHubSampleWebApp/PublicAPI/1.0.0"
     }
 
     return body, headers
 
 
-def stream_with_data(body, headers, endpoint):
-    logger.info(f"stream_with_data: {endpoint}")
+def stream_with_data(request):
     s = requests.Session()
     response = {
         "id": "",
@@ -203,24 +204,28 @@ def stream_with_data(body, headers, endpoint):
         }]
     }
 
-    logger.info("stream_with_data: starting POST with retries")
+    current_index = random.randint(0, 2)
     start_time = time.time()
     try:
         for retry in range(max_retries):
             try:
-                r = s.post(endpoint, json=body, headers=headers, stream=True, timeout=10)
+                logger.info(f"stream_with_data: starting POST with retries, index = {current_index}")
+                endpoint = generate_endpoint(current_index)
+                body, headers = prepare_body_headers_with_data(request.json["messages"], current_index)
+                r = s.post(endpoint, json=body, headers=headers, stream=True, timeout=5)
                 r.raise_for_status()
                 break
             except requests.exceptions.Timeout:
-                logger.error(f"POST timed out on attempt {retry}")
+                logger.error(f"stream_with_data: POST timed out on attempt {retry}")
                 if retry < max_retries - 1:
-                    logger.error("Retrying...")
+                    current_index = next_index(current_index)
+                    logger.error(f"stream_with_data: retrying, new index = {current_index}")
                 else:
-                    logger.error("Giving up and returning an error")
+                    logger.error("stream_with_data: giving up and returning an error")
                     yield json.dumps({"error": "Something went wrong, please try again."}) + "\n"
                     return
             except requests.exceptions.RequestException as e:
-                logger.error("An error occurred:", e)
+                logger.error("stream_with_data: an error occurred:", e)
                 yield json.dumps({"error": "Something went wrong, please try again."}) + "\n"
                 return
 
@@ -256,57 +261,22 @@ def stream_with_data(body, headers, endpoint):
             logger.info(f"stream_with_data: lines processed in {total_time} seconds")
     except Exception as e:
         logger.error(f"stream_with_data: exception processing response: {str(e)}")
-        # yield json.dumps({"error": "Something went wrong, please try again."}) + "\n"
+        yield json.dumps({"error": "Something went wrong, please try again."}) + "\n"
 
 
 def conversation_with_data(request):
-    current_index = random.randint(0, max_retries - 1)
-    for retry in range(max_retries):
-        try:
-            body, headers = prepare_body_headers_with_data(request, current_index)
-            openai_resource = openai_resources[current_index]
-            openai_model = openai_models[current_index]
-            endpoint = f"https://{openai_resource}.openai.azure.com/openai/deployments/{openai_model}/extensions/chat/completions?api-version={AZURE_OPENAI_PREVIEW_API_VERSION}"
-
-            if not SHOULD_STREAM:
-                r = requests.post(endpoint, headers=headers, json=body)
-                status_code = r.status_code
-                r = r.json()
-
-                return Response(json.dumps(r).replace("\n", "\\n"), status=status_code)
-            else:
-                if request.method == "POST":
-                    return Response(stream_with_data(body, headers, endpoint), mimetype='text/event-stream')
-                else:
-                    return Response(None, mimetype='text/event-stream')
-        except requests.exceptions.Timeout:
-            logger.error(f"POST timed out on attempt {retry}")
-            if retry < max_retries - 1:
-                logger.error("Retrying...")
-            else:
-                logger.error("Giving up and returning an error")
-                yield json.dumps({"error": "Something went wrong, please try again."}) + "\n"
-                return
-        except requests.exceptions.RequestException as e:
-            logger.error("An error occurred:", e)
-            yield json.dumps({"error": "Something went wrong, please try again."}) + "\n"
-            return
-
-
-
-def blah(request):
-    body, headers = prepare_body_headers_with_data(request)
-    endpoint = f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/openai/deployments/{AZURE_OPENAI_MODEL}/extensions/chat/completions?api-version={AZURE_OPENAI_PREVIEW_API_VERSION}"
-
     if not SHOULD_STREAM:
+        body, headers = prepare_body_headers_with_data(request.json["messages"], 0)
+        endpoint = generate_endpoint(0)
+        logger.info(f"Not streaming, using endpoint: {endpoint}")
         r = requests.post(endpoint, headers=headers, json=body)
         status_code = r.status_code
         r = r.json()
-
         return Response(json.dumps(r).replace("\n", "\\n"), status=status_code)
     else:
+        logger.info("Streaming, load balancing endpoints")
         if request.method == "POST":
-            return Response(stream_with_data(body, headers, endpoint), mimetype='text/event-stream')
+            return Response(stream_with_data(request), mimetype='text/event-stream')
         else:
             return Response(None, mimetype='text/event-stream')
 
